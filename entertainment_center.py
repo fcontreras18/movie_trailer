075503876478df04d7c585ca3a0795bfd9f37a52
Francisco Echeverria <fcontreras18@gmail.com>
2017-03-06 23:51:34 -0800

    Finishing up

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,34 +1,60 @@
 import fresh_tomatoes
 import media
+import omdb
+
+groundhog_omdb = omdb.get(title='Groundhog Day',
+                          year=1993, fullplot=True, tomatoes=True)
+what_we_do_omdb = omdb.get(title='What We Do In The Shadows',
+                           year=2014, fullplot=True, tomatoes=True)
+ex_machina_omdb = omdb.get(title='Ex Machina',
+                           year=2015, fullplot=True, tomatoes=True)
+goodfellas_omdb = omdb.get(title='Goodfellas',
+                           year=1990, fullplot=True, tomatoes=True)
+mistaken_for_strangers_omdb = omdb.get(title='Mistaken For Strangers',
+                                       year=2013, fullplot=True, tomatoes=True)
+no_country_omdb = omdb.get(title="No Country For Old Men",
+                           year=2007, fullplot=True, tomatoes=True)
     
 groundhog_day = media.Movie("Groundhog Day",
-                        "A reporter repeats the same day in a small town, forever.",
-                        "https://upload.wikimedia.org/wikipedia/en/b/b1/Groundhog_Day_%28movie_poster%29.jpg",
+                        groundhog_omdb.plot,
+                        groundhog_omdb.genre,
+                        groundhog_omdb.tomato_meter,
+                        groundhog_omdb.poster,
                         "https://www.youtube.com/watch?v=tSVeDx9fk60")
 
 what_we_do = media.Movie("What We Do In The Shadows",
-                     "A documentary crew follows around a group of vampires that live in a flat",
-                     "https://upload.wikimedia.org/wikipedia/en/7/70/What_We_Do_in_the_Shadows_poster.jpg",
+                     what_we_do_omdb.plot,
+                     what_we_do_omdb.genre,
+                     what_we_do_omdb.tomato_meter,
+                     what_we_do_omdb.poster,
                      "https://www.youtube.com/watch?v=IAZEWtyhpes")                    
 
 ex_machina = media.Movie("Ex Machina",
-                         "A man falls in love with an intelligent humanoid robot. Chaos ensues.",
-                         "https://upload.wikimedia.org/wikipedia/en/b/ba/Ex-machina-uk-poster.jpg",
+                         ex_machina_omdb.plot,
+                         ex_machina_omdb.genre,
+                         ex_machina_omdb.tomato_meter,
+                         ex_machina_omdb.poster,
                          "https://www.youtube.com/watch?v=bggUmgeMCdc")
 
 goodfellas = media.Movie("GoodFellas",
-                         "A Brooklyn kid is adopted by neighborhood gangsters.",
-                         "https://upload.wikimedia.org/wikipedia/en/7/7b/Goodfellas.jpg",
+                         goodfellas_omdb.plot,
+                         goodfellas_omdb.genre,
+                         goodfellas_omdb.tomato_meter,
+                         goodfellas_omdb.poster,
                          "https://www.youtube.com/watch?v=qo5jJpHtI1Y")
 
 mistaken_for_strangers = media.Movie("Mistaken For Stangers",
-                                     "An aspiring filmmaker documents his succesful older brother's band while he's a roadie.",
-                                     "http://www.audiofemme.com/wp-content/uploads/2014/04/mistaken-for-strangers.jpg",
+                                     mistaken_for_strangers_omdb.plot,
+                                     mistaken_for_strangers_omdb.genre,
+                                     mistaken_for_strangers_omdb.tomato_meter,
+                                     mistaken_for_strangers_omdb.poster,
                                      "https://www.youtube.com/watch?v=FNmprL3SOlM")
 
 no_country = media.Movie("No Country For Old Men",
-                         "A working man is on the run after finding $2 million dollars",
-                         "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
+                         no_country_omdb.plot,
+                         no_country_omdb.genre,
+                         no_country_omdb.tomato_meter,
+                         no_country_omdb.poster,
                          "https://www.youtube.com/watch?v=38A__WT3-o0")
 
 movies = [groundhog_day, what_we_do, ex_machina, goodfellas, mistaken_for_strangers, no_country]
--- media.py
+++ media.py
@@ -5,9 +5,11 @@ class Movie():
     
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
     
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, movie_genre, tomato_fresh, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
+        self.genre = movie_genre
+        self.tomato_fresh_score = tomato_fresh
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
